0b5cab3a3681082ae2b88f6302ae9214dd1b87bd
Andres Henriquez <rymbell@gmail.com>
2023-12-07 11:32:05 +0900

    added middleware validations for puts and posts requests

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { body, validationResult } from "express-validator";
+import { handleInputErrors } from "./modules/middleware";
 
 const router = Router();
 
@@ -11,15 +12,13 @@ router.get("/product", (req, res) => {
   res.json({ message: "OMG" });
 });
 router.get("/product/:id", () => {});
-router.put("/product/:id", body("name").isString(), (req, res) => {
-  const errors = validationResult(req);
-
-  if (!errors.isEmpty()) {
-    res.status(400);
-    res.json({ errors: errors.array() });
-  }
-});
-router.post("product", () => {});
+router.put(
+  "/product/:id",
+  body("name").isString(),
+  handleInputErrors,
+  (req, res) => {}
+);
+router.post("product", body("name").isString(), handleInputErrors, () => {});
 router.delete("product/:id", () => {});
 
 /*
@@ -28,8 +27,19 @@ update
 
 router.get("/update", () => {});
 router.get("/update/:id", () => {});
-router.put("/update/:id", () => {});
-router.post("update", () => {});
+router.put(
+  "/update/:id",
+  body("title").optional(),
+  body("body").optional(),
+  body("status").isIn(["IN_PROGRESS", "SHIPPED", "DEPRECATED"]),
+  body("version").optional(),
+  () => {}
+);
+router.post(
+  "update",
+  body("title").exists().isString(),
+  body("body").exists().isString()
+);
 router.delete("update/:id", () => {});
 
 /*
@@ -38,8 +48,19 @@ update point
 
 router.get("/updatepoint", (req, res) => {});
 router.get("/updatepoint/:id", () => {});
-router.put("/updatepoint/:id", () => {});
-router.post("updatepoint", () => {});
+router.put(
+  "/updatepoint/:id",
+  body("name").optional().isString(),
+  body("description").optional().isString(),
+  () => {}
+);
+router.post(
+  "updatepoint",
+  body("name").isString(),
+  body("description").isString(),
+  body("updateId").exists().isString(),
+  () => {}
+);
 router.delete("updatepoint/:id", () => {});
 
 export default router;
